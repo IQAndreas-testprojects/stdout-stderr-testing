2193b7efca3e67c49a96f9cbaa7bfd1c6a402bb9
Andreas Renberg (IQAndreas) <contact@iqandreas.com>
2015-09-09 20:22:45 -0500

    Add solution
    Solution provided by 200_success
     * http://stackoverflow.com/a/32491491/617937

--- swap-outputs.sh
+++ swap-outputs.sh
@@ -10,7 +10,11 @@
 #./stdout-test 1>&2
 
 # Display errors on the remote side, and output locally
-./stdout-test 1>&2 2>&1
+#./stdout-test 1>&2 2>&1
 # ERROR: This does not work! It sends everything to the local
 #  terminal (the one on the server)
+
+# Solution by 200_success: http://stackoverflow.com/a/32491491/617937
+# exec 3>&1- 1>&2- 2>&3- ; ./stdout-test
+./stdout-test 3>&1- 1>&2- 2>&3-
 
